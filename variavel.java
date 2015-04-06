9a2416527db59d142c65db95a80ac25e11cd140d
Marcelo Norberto <marcelonorberto07@gmail.com>
2015-04-06 18:16:25 -0300

    Classe de variavel pricipal

---
+++ variavel.java
@@ -0,0 +1,17 @@
+class Variavel{
+	private String nome;
+	public double valor;
+
+	public void setNome(String s){
+		this.nome = s;
+	}
+	public void setValor(double n){
+		this.valor = n;
+	}
+	public String getNome(){
+		return this.nome;
+	}
+	public double getValor(){
+		return this.valor;
+	}
+};
