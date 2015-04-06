3c7fb85db3bb5a37b6cae916c1ce3a4d9d6b6746
Marcelo Norberto <marcelonorberto07@gmail.com>
2015-04-06 18:18:06 -0300

    Classe que Analisa a linha do codigo de entrada, Variavelimite utilizada pela class analise.

---
+++ analise.java
@@ -0,0 +1,35 @@
+class Analise{
+
+	public static Variavelimite[] frase(String linha,Variavelimite[] lim){
+		String teste,aux;
+		aux = ";";
+		int i = 0;
+		teste = linha.substring(i,i+1);
+		while((teste.compareTo("aux")!=0 || teste.compareTo("{")!=0) && i<linha.length()){
+			if(teste.compareTo(" ")==0 && lim[0].valor==i){
+				lim[0].valor++;
+			}else if(teste.compareTo("	")==0 && lim[0].valor==i){
+				lim[0].valor++;
+			}else if(teste.compareTo("_")==0 && lim[0].valor==i){
+				lim[0].valor++;
+			}
+			if(teste.compareTo("(")==0){
+				aux = "{";
+			}
+			if(teste.compareTo(")")==0){
+				aux = ";";
+			}
+			i++;
+			if(i<linha.length()){
+				teste = linha.substring(i,i+1);
+			}else{
+				teste = linha.substring(i,linha.length());
+			}
+		}
+		lim[1].valor=i;
+		if(i==linha.length()){
+			System.out.println("Erro! Caracter de terminação não encontrado");
+		}
+		return lim;
+	}
+};
---
+++ variavelimite.java
@@ -0,0 +1,17 @@
+class Variavelimite{
+	private String nome;
+	public int valor;
+
+	public void setNome(String s){
+		this.nome = s;
+	}
+	public void setValor(int n){
+		this.valor = n;
+	}
+	public String getNome(){
+		return this.nome;
+	}
+	public int getValor(){
+		return this.valor;
+	}
+};
