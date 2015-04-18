a60bc93ca424097abee2c0467c9b483c0da7ae7f
Marcelo Norberto <marcelonorberto07@gmail.com>
2015-04-18 14:43:21 -0300

    A Classe declara.java é capaz de executar declarações de variaveis no programa.

--- Fonte/declara.java
+++ Fonte/declara.java
@@ -12,6 +12,7 @@ class Declara{
 		}
 		return null;
 	}
+	
 	public static void setVariavel(Variavel v,Variavel[] seq){
 		int aux;
 		for(aux=0;aux<seq.length;aux++){
@@ -22,5 +23,34 @@ class Declara{
 		}
 		System.out.println("Não é possivel inserir mais variaveis");
 		return;
+	}
+	
+	public static Variavel insere(String s,Variavel i,Variavel[] seq){
+		int aux;
+		String teste,f=null;
+		aux = s.length();
+		aux-=1;
+		while(aux>0){
+			teste = s.substring(aux-1,aux);
+			if(teste.compareTo(" ")!=0){
+				if(f==null){
+					f = teste;
+				}else{
+					f+=teste;
+				}
+			}else{
+				if(f!=null){
+					aux = -1;
+				}
+			}
+			aux--;
+		}
+		if(Declara.getVariavel(f,seq)==null){
+			i.setNome(f);
+			Declara.setVariavel(i,seq);
+			return Declara.getVariavel(f,seq);
+		}else{
+			return null;
+		}
 	}
 };
